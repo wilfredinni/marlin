2f625cd7eabb6845f20c6a9d73e15a9f15a7a31d
Carlos <carlos.w.montecinos@gmail.com>
2018-04-04 17:41:16 -0300

    logic

--- marlin/scripts/marlin.bat
+++ marlin/scripts/marlin.bat
@@ -1,34 +1,25 @@
-@ECHO OFF
-set bookmark=%1
-if [%bookmark%]==[] goto :none
-if %bookmark%==do-pack goto :do-pack
-if %bookmark%==desktop goto :desktop
-if %bookmark%==uselessbot goto :uselessbot
-if %bookmark%==marlin goto :marlin
-if %bookmark%==root goto :root
-if %bookmark%==projects goto :projects
-if %bookmark%==carlos goto :carlos
-:none
-echo hola mundo
-exit /b 0
-:do-pack
-cd c:\Dropbox\Dropbox\projects\python\do-pack
-exit /b 0
-:desktop
-cd c:\Users\carlo\Desktop
-exit /b 0
-:uselessbot
-cd c:\Dropbox\Dropbox\projects\python\retwiBot
-exit /b 0
-:marlin
-cd C:\Dropbox\Dropbox\projects\python\marlin
-exit /b 0
-:root
-cd c:\
-exit /b 0
-:projects
-cd c:\Dropbox\Dropbox\projects\python
-exit /b 0
-:carlos
-cd c:\Users\carlo
-exit /b 0
+@echo off
+set bookmark_name=%1
+
+
+
+if not exist "%USERPROFILE%\marlin" (
+    md  %USERPROFILE%\marlin
+    echo creada carpeta de marlin
+)
+
+if [%bookmark_name%]==[] goto :HELP
+
+if exist "%USERPROFILE%\marlin\%bookmark_name%" (
+    set /p bname=<%USERPROFILE%\marlin\%bookmark_name%
+    cd %bname%
+    goto :END
+) else (
+    echo no existe el archivo %bookmark_name%
+    goto :END
+)
+
+:HELP
+python marlin_help.py
+
+:END
