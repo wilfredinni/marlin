fa14793cb608b0ddf5a970124b9202a39e4e509a
Carlos <carlos.w.montecinos@gmail.com>
2018-04-04 18:11:24 -0300

    minor fixes

--- marlin/manage.py
+++ marlin/manage.py
@@ -9,11 +9,3 @@ class ManageBookmark:
 
     def __init__(self, json_file):
         self.json_file = json_file
-
-    def read_json(self):
-        with open(self.json_file, 'r') as f:
-            return json.load(f)
-
-    def add_bookmark(self, content):
-        with open(self.json_file, 'w') as f:
-            json.dump(content, f)
