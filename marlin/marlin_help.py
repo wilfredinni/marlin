ace230c235b83c7fb12a421a99d51c01013dc4b0
Carlos <carlos.w.montecinos@gmail.com>
2018-04-10 18:17:53 -0300

    Add styles for cli and help

---
+++ marlin/marlin_help.py
@@ -0,0 +1,47 @@
+from marlin.manage import ManageBookmark
+from marlin.styles import label
+import click
+
+commands = {
+    'bookmark': '{} {} {}'.format(
+        label('good'),
+        'bookmark'.ljust(15),
+        'Bookmark current Folder'
+    ),
+    'rmark': '{} {} {}'.format(
+        label('bad'),
+        'rmark'.ljust(15),
+        'Remove a Bookmark'
+    ),
+    'marlin': '{} {} {}'.format(
+        label('run'),
+        'marlin'.ljust(15),
+        'Swim through the Terminal!\n'
+    )
+}
+
+
+def main():
+    click.echo('Usage: COMMAND [bookmark-name]\n')
+    click.echo('Commands:')
+    for command in commands:
+        click.echo(commands[command])
+
+    list_bookmarks()
+
+
+def list_bookmarks():
+    bookmark_object = ManageBookmark('mock', 'mock')
+    all_bookmarks = bookmark_object.list_bookmark()
+    click.echo('Saved Bookmarks:')
+    for bookmark in all_bookmarks:
+        bookmark_path = bookmark_object.read_bookmark(bookmark)
+        click.echo('{} {} {}'.format(
+            label('list'),
+            bookmark.ljust(17),
+            bookmark_path
+        ))
+
+
+if __name__ == '__main__':
+    main()
---
+++ marlin/styles.py
@@ -0,0 +1,23 @@
+from colorama import init
+from colorama import Fore, Style
+
+init(autoreset=True)
+
+
+def label(label):
+    labels = {
+        'info': Fore.YELLOW + Style.BRIGHT + '[!]' + Style.RESET_ALL,
+        'bad': Fore.RED + Style.BRIGHT + '[-]' + Style.RESET_ALL,
+        'good': Fore.GREEN + Style.BRIGHT + '[+]' + Style.RESET_ALL,
+        'run': Fore.WHITE + Style.BRIGHT + '[~]' + Style.RESET_ALL,
+        'list': Fore.YELLOW + Style.BRIGHT + '>' + Style.RESET_ALL
+    }
+    return labels[label]
+
+
+def color(color, text):
+    colors = {
+        'yellow': Fore.YELLOW + Style.BRIGHT + text + Style.RESET_ALL,
+        'red': Fore.RED + Style.BRIGHT + text + Style.RESET_ALL
+    }
+    return colors[color]
