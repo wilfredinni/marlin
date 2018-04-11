8580ab002892e4688639d6dba9f5d8a4d16c89cb
Carlos <carlos.w.montecinos@gmail.com>
2018-04-10 22:15:11 -0300

    Revert "Add tests"
    This reverts commit 4f39aa864d47d39251778e0752de55dfd9dcf37c.

--- tests/test_marlin.py
+++ tests/test_marlin.py
@@ -1,36 +1,38 @@
-from marlin.manage import ManageBookmark
-from pathlib import Path
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-# new test path
-marlin_path = Path((Path.home()) / '.test_marlin')
-mock_object = ManageBookmark('mock', 'mock_path', marlin_path)
+"""Tests for `marlin` package."""
 
+import pytest
 
-def test_create_marlin_folder():
-    mock_object.create_marlin_folder()
-    exists = Path(marlin_path).exists()
-    assert exists is True
+from click.testing import CliRunner
 
-
-def test_add_bookmark():
-    mock_object.add_bookmark()
-    bookmark = Path((marlin_path) / 'mock')
-    exists = Path(bookmark).exists()
-    assert exists is True
+from marlin import marlin
+from marlin import cli
 
 
-def test_read_bookmark():
-    mock_path = mock_object.read_bookmark('mock')
-    assert mock_path == 'mock_path'
+@pytest.fixture
+def response():
+    """Sample pytest fixture.
+
+    See more at: http://doc.pytest.org/en/latest/fixture.html
+    """
+    # import requests
+    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
 
 
-def test_list_bookmark():
-    list_bookmark = mock_object.list_bookmark()
-    assert isinstance(list_bookmark, list)
+def test_content(response):
+    """Sample pytest test function with the pytest fixture as an argument."""
+    # from bs4 import BeautifulSoup
+    # assert 'GitHub' in BeautifulSoup(response.content).title.string
 
 
-def test_remove_bookmark():
-    mock_object.remove_bookmark()
-    bookmark = Path((marlin_path) / 'mock')
-    exists = Path(bookmark).exists()
-    assert exists is False
+def test_command_line_interface():
+    """Test the CLI."""
+    runner = CliRunner()
+    result = runner.invoke(cli.main)
+    assert result.exit_code == 0
+    assert 'marlin.cli.main' in result.output
+    help_result = runner.invoke(cli.main, ['--help'])
+    assert help_result.exit_code == 0
+    assert '--help  Show this message and exit.' in help_result.output
