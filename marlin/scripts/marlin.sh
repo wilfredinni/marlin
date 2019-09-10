d5364a9707e3b2727092a651b27504f999ea2b7e
Pranav Void <pranavvoid@pm.me>
2019-09-10 16:26:43 +0530

    created the marlin function

--- marlin/scripts/marlin.sh
+++ marlin/scripts/marlin.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
-bookmark_name=$1
 
-if [ ! -d "$HOME/.marlin" ]; then
-    mkdir "$HOME/.marlin"
-fi
+marlin() {
+    bookmark_name=$1
 
-if [ -z "$bookmark_name" ]; then
-    marlin_help
-    exit 0
-fi
+    if [ ! -d "$HOME/.marlin" ]; then
+        mkdir "$HOME/.marlin"
+    fi
 
-if [ -f "$HOME/.marlin/$bookmark_name" ]; then
-    bname=$(cat "$HOME/.marlin/$bookmark_name")
-    cd "$bname" || exit
-else
-    echo "Bookmark does not exists: $bookmark_name"
-fi
+    if [ -z "$bookmark_name" ]; then
+        marlin_help
+        exit 0
+    fi
 
+    if [ -f "$HOME/.marlin/$bookmark_name" ]; then
+        bname=$(cat "$HOME/.marlin/$bookmark_name")
+        cd "$bname" || exit
+    else
+        echo "Bookmark does not exists: $bookmark_name"
+    fi
+}
